90d38a9b787c4485b5f5465af6f8f21f880202c7
Burak KARADAŞ <burak@atilimyazilim.com>
2019-12-31 14:35:23 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace YandexSearchAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.Link});
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv.Location = new System.Drawing.Point(0, 0);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.Size = new System.Drawing.Size(1225, 562);
+            this.dgv.TabIndex = 0;
+            // 
+            // Title
+            // 
+            this.Title.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Title.HeaderText = "Title";
+            this.Title.Name = "Title";
+            this.Title.ReadOnly = true;
+            this.Title.Width = 52;
+            // 
+            // Link
+            // 
+            this.Link.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            this.Link.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1225, 562);
+            this.ControlBox = false;
+            this.Controls.Add(this.dgv);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yandex Search API";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Link;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace YandexSearchAPI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string GetValue(XElement group, string name)
+        {
+            try
+            {
+                return group.Element("doc").Element(name).Value;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServicePointManager.Expect100Continue = false;
+
+            string aranacak = "c#";
+            string useranme = "goren-ali";
+            string key = "03.178948983:5f00fe5e625cdd51ef5819e61107cc0a";
+            string yandexurl = @"https://yandex.com.tr/search/xml?user=" + useranme + @"&key=" + key + @"&lr=983&l10n=tr&filter=none";
+
+            // Текст запроса в формате XML
+            string command =
+              @"<?xml version=""1.0"" encoding=""UTF-8""?>   
+          <request>   
+           <query>" + aranacak + @"</query>
+           <page>" + 0 + @"</page>
+           <groupings>
+             <groupby attr=""d"" 
+                    mode=""deep"" 
+                    groups-on-page=""10"" 
+                    docs-in-group=""1"" />   
+           </groupings>   
+          </request>";
+
+            byte[] bytes = Encoding.UTF8.GetBytes(command);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(yandexurl);
+            request.Method = "POST";
+            request.ContentLength = bytes.Length;
+            request.ContentType = "text/xml";
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(bytes, 0, bytes.Length);
+            }
+
+            // Получаем ответ
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            XmlReader xmlReader = XmlReader.Create(response.GetResponseStream());
+            XDocument xmlResponse = XDocument.Load(xmlReader);
+
+            var groupQuery = from gr in xmlResponse.Elements().
+                Elements("response").
+                Elements("results").
+                Elements("grouping").
+                Elements("group")
+                             select gr;
+
+              
+
+            for (int h = 0; h < groupQuery.Count(); h++)
+            {
+                dgv.Rows.Add(GetValue(groupQuery.ElementAt(h), "title"), GetValue(groupQuery.ElementAt(h), "url"));
+             
+            }
+        }
+    }
+}
